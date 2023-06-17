25bb93a1db60ba0f553a9ba70b45dfe1246b8b2f
Kirill <kirSM2010@gmail.com>
2023-06-18 01:24:24 +0300

    Added signed shifter module

---
+++ signed_shifter.v
@@ -0,0 +1,11 @@
+module signed_shifter (input wire [`ITERATION_BITS-1:0] i,
+                       input wire signed [`XY_BITS:0] D,
+                       output reg signed [`XY_BITS:0] Q);
+integer j;
+always @ *
+begin
+Q = D;
+for(j = 0;j<i;j = j+1)
+    Q = (Q >> 1) | (D[`XY_BITS] << `XY_BITS); // Shift right by 1 bit "i" times - keep the sign
+end
+endmodule
